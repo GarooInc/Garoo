ec6c55f9ea2ba35881a6c80800a4013300790b40
Jimena Hernández <her21199@uvg.edu.gt>
2023-10-05 08:02:11 -0600

    videos

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -20,7 +20,7 @@ const Portfolio = () => {
         </div>
         <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
           <video className="w-[48rem] max-w-none h-full rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] object-cover" loop muted autoPlay playsInline>
-            <source src="https://cdn.shopify.com/videos/c/o/v/ab3abd9ac2f74111968ff28fbaa2a1a0.mp4" type="video/mp4"/>
+            <source src="https://cdn.shopify.com/videos/c/o/v/06c704982afd46e58389bf28425e5b70.mp4" type="video/mp4"/>
           </video>
         </div>
       </div>
