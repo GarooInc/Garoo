680dbcc14fcc99bfdccc77341c064ab4b7b64765
Jimena Hernández <her21199@uvg.edu.gt>
2023-10-13 12:30:01 -0600

    Update LogoTestimonials.jsx

--- src/components/LogoTestimonials/LogoTestimonials.jsx
+++ src/components/LogoTestimonials/LogoTestimonials.jsx
@@ -9,7 +9,7 @@ const LogoTestimonials = () => {
         src : "/images/testimonials/1.png",
       },
       {
-        text: "“ They are very responsive and professional. ”",
+        text: "“ They are very responsive and professional.”",
         src : "/images/testimonials/2.png",
       },
       {
@@ -22,16 +22,14 @@ const LogoTestimonials = () => {
       <div className="bg-white py-24 sm:py-32">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="marquee-container">
-          <div className="mx-auto mt-10 flex items-center gap-x-10 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-16 lg:mx-0 lg:max-w-none lg:grid-cols-5 marquee">
+          <div className="mx-auto mt-10 flex items-center md:gap-x-20 gap-x-10 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-16 lg:mx-0 lg:max-w-none lg:grid-cols-5 marquee">
             {testimonials.map((testimonial) => (
                 <a key={testimonial.src} className="flex items-center gap-2 mb-8 mt-8 min-w-xl md:items-center mx-10 md:mx-0">
                 <p className="text-garoo-sky text-center  text-xl md:text-2xl font-bold tracking-tight w-full whitespace-nowrap">{testimonial.text}</p>
                 <img
                     src={testimonial.src}
                     alt=""
-                    className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-                    width={100}
-                    height={80}
+                    className="col-span-2 object-contain lg:col-span-1 max-h-12 w-12 md:w-20 md:w-14"
                 />
               </a>
                 ))
